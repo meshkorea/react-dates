553cacacdb6ef593d49c1dc857d52a410a5d48b0
Tae-geon Choi <contact@zfbe.com>
2018-03-02 18:57:12 +0900

    Fix autoformatting logic

--- src/components/SingleDatePicker.jsx
+++ src/components/SingleDatePicker.jsx
@@ -162,15 +162,7 @@ class SingleDatePicker extends React.Component {
       onFocusChange,
       onClose,
     } = this.props;
-    const escaped = dateString.replace(/\D/g, '');
-    const formattedString = [
-      escaped.slice(0, 4),
-      escaped.slice(4, 2),
-      escaped.slice(6, 2),
-    ]
-      .filter(x => x)
-      .join('-');
-    const newDate = toMomentObject(formattedString, this.getDisplayFormat());
+    const newDate = toMomentObject(dateString, this.getDisplayFormat());
 
     const isValid = newDate && !isOutsideRange(newDate);
     if (isValid) {
